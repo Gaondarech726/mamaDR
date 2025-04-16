4457ba3e41b1f4ecf7291d59950aee9b269ff994
Anton Honcharenko <123805187+Gaondarech726@users.noreply.github.com>
2025-04-16 16:33:22 +0300

    pack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 import BirthdayPage from './BirthdayPage.jsx';
-import Slider from '../components/Slider/Slider.jsx';
 
 export const App = () => {
   return (
